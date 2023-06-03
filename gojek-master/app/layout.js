14094eb3f25cf941f76be3e005374a7d6982c3e2
khairulihsannudin <khairulihsannudin@mail.ugm.ac.id>
2023-06-03 19:42:16 +0700

    Head edit

--- gojek-master/app/layout.js
+++ gojek-master/app/layout.js
@@ -2,10 +2,15 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Gojek Super App: Ojek Online, Taksi Online, Pesan Makan, Kirim Barang, Pembayaran</title>
+        <link
+          rel="icon"
+          href="icon.svg"
+        />
+      </head>
       <body>
-        <div className="m-0">
-          {children}
-        </div>
+        <div className="m-0">{children}</div>
       </body>
     </html>
   );
